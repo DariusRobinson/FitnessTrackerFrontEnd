6dcdb1f28bdff14924f42aad7de78852d28d037f
Darius Robinson <robinson.darius090@gmail.com>
2022-08-03 10:00:37 -0400

    working on activity forms

--- src/components/Activities.js
+++ src/components/Activities.js
@@ -1,13 +1,9 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { getAllActivities } from "../api";
 import ActiviyForm from "./ActivityForm";
 
-
-
-
 const Activities = ({ allActivities, setAllActivities, token }) => {
-
-    
+  const [active, setActive] = useState(false);
 
   const fetchActivities = async () => {
     const activityList = await getAllActivities();
@@ -20,27 +16,45 @@ const Activities = ({ allActivities, setAllActivities, token }) => {
   }, []);
 
   return (
-    <> 
-    { token ? 
     <>
-    <button onClick={()=>{
-        return <div><ActiviyForm /></div>
-    }}>Create New Activity</button>
-    </> 
-    : <></> }
+      {token ? (
+        <>
+          {" "}
+          {active ? (
+            <>
+              <ActiviyForm />
+              <button
+                className="activityFormCancelButton"
+                onClick={() => {
+                  setActive(false);
+                }}
+              >
+                Cancel
+              </button>
+            </>
+          ) : (
+            <button
+              className="activityFormButton"
+              onClick={() => {
+                setActive(true);
+              }}
+            >
+              Create New Activity
+            </button>
+          )}
+        </>
+      ) : (
+        <></>
+      )}
 
-
-
-
-       {allActivities.map((element, index) => {
+      {allActivities.map((element, index) => {
         return (
           <div key={index} className="activities">
             <h4 className="activityName">{element.name}</h4>
             <p className="activityDescription">{element.description}</p>
           </div>
-        ); 
+        );
       })}
-      
     </>
   );
 };
